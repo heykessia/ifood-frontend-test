ffc84aaead0433d4aab18ae05c4438a920f66323
Késsia Castro <kessiacastro@gmail.com>
2020-10-14 18:34:41 -0300

    add react router dom to entry file

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 
-const Home = () => {
+const Login = () => {
   return (
     <Header />
   );
 }
 
-export default Home;
+export default Login;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../../Pages/Home/Home'
+import Login from '../../Pages/Login/Login'
+
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../utils/theme';
-import Header from '../Header/Header';
 
 const App = () => {
   return (
-      <MuiThemeProvider theme={theme}>
-        <ThemeProvider theme={theme}>
-          <Header />
-        </ThemeProvider>
-      </MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Login} exact />
+            <Route path="/dashboard" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    </MuiThemeProvider>
   );
 }
 
